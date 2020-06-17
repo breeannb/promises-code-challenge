cb0fba28629321de8e7413a273627137ac6e749d
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-17 13:54:00 -0700

    able to make copy file, the first function for copy

--- sandbox.js
+++ sandbox.js
@@ -3,17 +3,28 @@
 //      require const fsPromises = require('fs').promises;
 const fs = require('fs').promises;
 //      use fsPromises.readFile to read a file
+// const promiseToReadFile = fs.readFile('./README.md', { encoding: 'utf8' });
+// //      then console.log the file
+// promiseToReadFile
+//   .then(result => {
+//     console.log(result);
+//   }); 
+
+// // Write a file
+// //      use fsPromises.writeFile to write a file
+// const promiseToWriteFile = fs.writeFile('./my-new-written-file.txt', 'this is the new text file');
+
+// // use fsPromises.writeFile to write a file
+// // then console.log "DONE!"
+// promiseToWriteFile.then(() => console.log('DONE!')); 
+
+
+// Copy a file
+//      use fsPromises.readFile to read a file
 const promiseToReadFile = fs.readFile('./README.md', { encoding: 'utf8' });
-//      then console.log the file
-promiseToReadFile
-  .then(result => {
-    console.log(result);
-  }); 
+//      then use fsPromises.writeFile to write the read data
+const promiseToWriteFile = fs.writeFile('./my-copy-of-README.md', 'this is the copy of README file');
+//      then console.log "DONE!"
+Promise.all([promiseToReadFile, promiseToWriteFile]).then(() => 
+  console.log('DONE!'));
 
-// Write a file
-//      use fsPromises.writeFile to write a file
-const promiseToWriteFile = fs.writeFile('./my-new-written-file.txt', 'this is the new text file');
-
-// use fsPromises.writeFile to write a file
-// then console.log "DONE!"
-promiseToWriteFile.then(() => console.log('DONE!')); 
