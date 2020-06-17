ade01d72a9dc41f5b049255426da165998130f99
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-17 11:59:24 -0700

    creating some files to get started

---
+++ sandbox.js
@@ -0,0 +1,15 @@
+// Read a file
+//      require const fsPromises = require('fs').promises;
+//      use fsPromises.readFile to read a file
+//      then console.log the file
+
+
+
+
+
+
+// Write a file
+//      require const fsPromises = require('fs').promises;
+//      use fsPromises.writeFile to write a file
+//      then console.log "DONE!"
+
